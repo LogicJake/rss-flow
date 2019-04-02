f3f3b4eb20217f60474d004a88795fc0af6c67ae
Chenyang Shi <chenyang9608@qq.com>
2019-04-02 15:25:47 +0800

    no cross

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # @Author: LogicJake
 # @Date:   2019-02-15 19:33:23
-# @Last Modified time: 2019-02-15 20:06:40
+# @Last Modified time: 2019-04-02 15:25:00
 from flask import Flask
 from app.config import config
 from app.blueprints.main import bp as main_bp
+
+
+def after_request(resp):
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
 
 
 def create_app(config_name):
@@ -12,4 +17,5 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
 
     app.register_blueprint(main_bp)
+    app.after_request(after_request)
     return app
